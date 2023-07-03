ecb3ac007f992d61063f6f78abfe9d273ee504ae
Woody <damiandrewniany@gmail.com>
2023-07-03 12:06:19 +0200

    1. Beginning

---
+++ Szopi/src/main/java/com/woody/Szopi/SzopiApplication.java
@@ -0,0 +1,13 @@
+package com.woody.Szopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzopiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzopiApplication.class, args);
+	}
+
+}
---
+++ Szopi/src/test/java/com/woody/Szopi/SzopiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woody.Szopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzopiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
